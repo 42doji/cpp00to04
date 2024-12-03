d8f27dcb7f445a43d93b175e659b0436ed3c81fa
42doji <doji@student.42gyeongsan.kr>
2024-12-03 13:27:13 +0900

    ex01zombienumbering

--- cpp01/ex01/srcs/ZombieHorde.cpp
+++ cpp01/ex01/srcs/ZombieHorde.cpp
@@ -1,9 +1,16 @@
 #include "../includes/Zombie.hpp"
+#include <sstream>
 
 Zombie	*ZombieHorde(int N, std::string name)
 {
 	Zombie	*zombies = new Zombie[N];
+    std::stringstream ss;
+
 	for (int i = 0; i < N; i++)
-		zombies[i].setName(name);
+    {
+        ss.str("");
+        ss << i;
+		zombies[i].setName(name + ss.str());
+	}
 	return (zombies);
 }
--- cpp01/ex01/srcs/main.cpp
+++ cpp01/ex01/srcs/main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include <string>
-#include <sstream>
+
 #include "Zombie.hpp"
 #define N 4
 
 int main( void )
 {
-	Zombie *zombiz = ZombieHorde(N, "zombi");
+	Zombie *zombiz = ZombieHorde(N, "zombieHorde");
 
 	for (size_t i = 0; i < N; i++)
 	{
-		std::stringstream ss;
-		ss << i;
-		zombiz[i].setName("zombi" + ss.str());
 		zombiz[i].announce();
 	}
 
